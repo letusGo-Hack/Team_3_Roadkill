7de5356638e662a0d04031e9f59b043bc2dc1cd5
홍서연 <hongssup0202@gmail.com>
2024-06-29 18:16:55 +0900

    녹음 중 lottie 추가

--- Roadkill/Roadkill/Views/RecordView.swift
+++ Roadkill/Roadkill/Views/RecordView.swift
@@ -6,10 +6,32 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct RecordView: View {
+    @State var isRecording: Bool = false
+    var recordingLottie = LottieView(animation: .named("recording"))
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 12) {
+            if isRecording {
+                recordingLottie
+                    .playing(loopMode: .loop)
+                    .onTapGesture {
+                        isRecording.toggle()
+                    }
+            } else {
+                recordingLottie
+                    .playbackMode(.paused)
+                    .onTapGesture {
+                        isRecording.toggle()
+                    }
+            }
+            Text(isRecording ? "녹음 중입니다..." : "")
+                .foregroundColor(.black)
+        }
+        .padding(.bottom, 20)
+        
     }
 }
 
--- Roadkill/Roadkill/Views/ReportListView.swift
+++ Roadkill/Roadkill/Views/ReportListView.swift
@@ -6,10 +6,40 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ReportListView: View {
+    @State private var reports: [Report] = [
+            Report(date: "2024-06-29", location: "서울", text: "음성 녹음 내용", summary: "요약", isReported: true),
+            Report(date: "2024-06-28", location: "경기", text: "음성 녹음 내용", summary: "요약", isReported: true),
+            Report(date: "2024-06-27", location: "부산", text: "음성 녹음 내용", summary: "요약", isReported: false)
+        ]
+    
+    @ViewBuilder
     var body: some View {
-        Text("Hello, World!")
+        Text("신고 내역")
+            .font(.headline)
+        
+        List(reports, id: \.self) { report in
+            VStack(alignment: .leading, spacing: 4) {
+                HStack() {
+                    Text("\(report.date)")
+                        .font(.headline)
+                    Spacer()
+                    Image(systemName: report.isReported ? "checkmark.square.fill" :"checkmark.square")
+                        .foregroundColor(.blue)
+                }
+                
+                Text("장소: \(report.location)")
+                    .font(.body)
+                Text("내용: \(report.text)")
+                    .font(.body)
+                Text("요약: \(report.summary)")
+                    .font(.body)
+            }
+        }
+        .listStyle(.plain)
+        .padding(.top, 8)
     }
 }
 
