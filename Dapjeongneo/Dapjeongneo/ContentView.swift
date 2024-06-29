762ac260757beab7cd65defe76275f6440ff710f
Junnos  <junnos.dev@gmail.com>
2024-06-29 11:22:49 +0900

    ⛵ 프로젝트 생성

---
+++ Dapjeongneo/Dapjeongneo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dapjeongneo
+//
+//  Created by 이창준 on 6/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dapjeongneo/Dapjeongneo/DapjeongneoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DapjeongneoApp.swift
+//  Dapjeongneo
+//
+//  Created by 이창준 on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct DapjeongneoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
