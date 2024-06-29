5d1ee03873f56194a00de1bc82b7398b16d3fe27
lucymoon-d <lucymoon.d@gmail.com>
2024-06-29 17:18:35 +0900

    IntroductionView UI 작업

--- Roadkill/Roadkill/Views/IntroductionView.swift
+++ Roadkill/Roadkill/Views/IntroductionView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct IntroductionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            Text("로드킬 신고를 위한 음성을 남겨주세요. 🙌\n해당 정보를 참고하여 남겨주시기 바랍니다.")
+                .font(.headline)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("1. 위치 📍")
+                    .font(.headline)
+                
+                Text("• 위치에 대한 추가 설명\n• 주행 방향\n• 로드킬 차선 위치")
+                    .font(.caption)
+            }
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("2. 사체 정보")
+                    .font(.headline)
+                
+                Text("• 동물 종류\n• 형태 확인 가능 여부")
+                    .font(.caption)
+            }
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("3. 발견 시간 🕐")
+                    .font(.headline)
+            }
+        }
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 
